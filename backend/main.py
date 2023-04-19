5cdd75787c8654d4a272ffbead6e050395248c7d
NuoWenLei <nuowen0612@gmail.com>
2023-04-18 23:49:00 -0400

    add python backend todos

--- backend/main.py
+++ backend/main.py
@@ -6,4 +6,11 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+# Endpoints TODO:
+# - Cluster Results (Stored on Linode of cluster results and what algo is used)
+# - Index of interest group embeddings with index of interest group names on Linode
+# 	- updated daily
+# - Interest Group Person Match
+# - Interest Group Name Search
+
 # @app.get("/")
