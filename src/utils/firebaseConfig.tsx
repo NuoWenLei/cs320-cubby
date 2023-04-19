31da347281535a923e304bfe7bd552692e1a50bc
NuoWenLei <nuowen0612@gmail.com>
2023-04-18 23:20:43 -0400

    add reqs and firebase configs for typescript and python

---
+++ src/utils/firebaseConfig.tsx
@@ -0,0 +1,12 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from "firebase/storage";
+import firebaseConfig from "../../private/firebaseCreds.json";
+// import firebase from 'firebase'
+// import 'firebase/firestore'
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const firestore = getFirestore(app);
+export const storage = getStorage();
