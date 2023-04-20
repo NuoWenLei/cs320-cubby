640540ad6582030fb53e57b8a59bb713b24a7a26
NuoWenLei <nuowen0612@gmail.com>
2023-04-20 16:56:50 -0400

    foo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,15 @@ import Layout from '@/components/layouts'
 import '@/styles/globals.css'
 import { FirebaseAuthContext, useFirebaseAuth } from '@/utils/firebaseFunctions'
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   const firebaseAuthState = useFirebaseAuth();
   return (
     <FirebaseAuthContext.Provider value={firebaseAuthState}>
+      <Head>
+        <title>Cubby</title>
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
