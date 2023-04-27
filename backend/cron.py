6c1d483386abf0146ba04797f0ebcb42eaadc66f
NuoWenLei <nuowen0612@gmail.com>
2023-04-27 13:43:04 -0400

    f

--- backend/cron.py
+++ backend/cron.py
@@ -42,65 +42,66 @@ async def main():
 	for f, c in zip(unique_features.tolist(), (feature_counts.astype("float32") / NUM_FEATURES).tolist()):
 		all_unique_features[f] = round(c, 3) 
 
+	print(all_unique_features)
 
 	# TODO: Do we filter out new groups with only 1 match?
 	# group_indices[counts >= MIN_MEMBER_PER_GROUP]
 
-	index2group_id = {}
-	for group_index in group_indices:
+	# index2group_id = {}
+	# for group_index in group_indices:
 
-		# create group
-		group_id = await add_doc("groups", {
-			"friend_group": True,
-			"feature_dist": all_unique_features.tolist(),
-			"member_ids": []
-		})
+	# 	# create group
+	# 	group_id = await add_doc("groups", {
+	# 		"friend_group": True,
+	# 		"feature_dist": all_unique_features.tolist(),
+	# 		"member_ids": []
+	# 	})
 
-		index2group_id[str(int(group_index))] = group_id
+	# 	index2group_id[str(int(group_index))] = group_id
 		
-		# find users matched with group
-		matched_indices = (cluster_model.suggestions == group_index).sum(axis = 1) > 0
+	# 	# find users matched with group
+	# 	matched_indices = (cluster_model.suggestions == group_index).sum(axis = 1) > 0
 
-		if len(matched_indices) == 0:
-			continue
+	# 	if len(matched_indices) == 0:
+	# 		continue
 
-		# get corresponding user id
-		matched_user_ids = user_ids[matched_indices]
+	# 	# get corresponding user id
+	# 	matched_user_ids = user_ids[matched_indices]
 
-		# get corresponding user suggestions
-		matched_user_suggestions = cluster_model.suggestions[matched_indices]
+	# 	# get corresponding user suggestions
+	# 	matched_user_suggestions = cluster_model.suggestions[matched_indices]
 
-		# get corresponding similarity scores
-		matched_similarities = cluster_model.sims[matched_indices]
+	# 	# get corresponding similarity scores
+	# 	matched_similarities = cluster_model.sims[matched_indices]
 
-		# add group invitations for each user matched
-		for _id, matched_suggestions, sims in zip(
-			matched_user_ids.tolist(),
-			matched_user_suggestions,
-			matched_similarities):
+	# 	# add group invitations for each user matched
+	# 	for _id, matched_suggestions, sims in zip(
+	# 		matched_user_ids.tolist(),
+	# 		matched_user_suggestions,
+	# 		matched_similarities):
 
-			print({
-				"user_id": _id,
-				"group_id": group_id,
-				"status": "pending",
-				"similarity_matched": round(sims[matched_suggestions == group_index][0], 3)
-			})
+	# 		print({
+	# 			"user_id": _id,
+	# 			"group_id": group_id,
+	# 			"status": "pending",
+	# 			"similarity_matched": round(sims[matched_suggestions == group_index][0], 3)
+	# 		})
 
-			# add new invitiation 
-			await add_doc("invitations", {
-				"user_id": _id,
-				"group_id": group_id,
-				"status": "pending",
-				"similarity_matched": round(sims[matched_suggestions == group_index][0], 3)
-			})
+	# 		# add new invitiation 
+	# 		await add_doc("invitations", {
+	# 			"user_id": _id,
+	# 			"group_id": group_id,
+	# 			"status": "pending",
+	# 			"similarity_matched": round(sims[matched_suggestions == group_index][0], 3)
+	# 		})
 
-	print(f"Index to group id: {index2group_id}")
+	# print(f"Index to group id: {index2group_id}")
 
-	# Locally store index-to-group_firebase_id map
-	store_path = os.path.join(os.path.dirname(__file__), "cluster/index_to_group_id.json")
+	# # Locally store index-to-group_firebase_id map
+	# store_path = os.path.join(os.path.dirname(__file__), "cluster/index_to_group_id.json")
 
-	with open(store_path, "w") as store_json:
-		json.dump(index2group_id, store_json)
+	# with open(store_path, "w") as store_json:
+	# 	json.dump(index2group_id, store_json)
 	
 
 if __name__ == "__main__":
