0ddb29ac4931dc45c25f3b4c22ba411b50b3b5f6
NuoWenLei <nuowen0612@gmail.com>
2023-04-27 15:14:31 -0400

    implement matches page

--- src/components/Matchbar.tsx
+++ src/components/Matchbar.tsx
@@ -29,15 +29,15 @@ export default function Matchbar(
 				/>
 			</div>
 			<div className="basis-10/12 overflow-x-scroll">
-			<div className="flex flex-row w-full text-black m-5">
+			<div className="flex flex-row w-full text-orange-950 m-5">
 					{
 						items.map((item: number) => {
 							return (
 								<div
 								onClick={() => {setIndex(1)}}
-								className={(index == 1 ? "border-2 border-black " : "") + " w-36 shrink-0 flex flex-col mx-4 p-4 cursor-pointer"}>
-									<div className="mx-auto text-center text-2xl">
-										<div className="h-20 w-20 rounded-full overflow-hidden mb-4">
+								className={(index == 1 ? "border-orange-950 " : "border-transparent ") + "border-2 w-18 md:w-32 shrink-0 flex flex-col mr-6 md:mr-12 py-2 cursor-pointer"}>
+									<div className="mx-auto text-center text-xl md:text-2xl">
+										<div className="h-12 w-12 md:h-20 md:w-20 rounded-full overflow-hidden mb-2 md:mb-4">
 											<img src="https://www.foodnetwork.com/content/dam/images/food/fullset/2019/2/19/1/FN_Air-Fryer-Chicken-Wings-H_s4x3.jpg"
 											className="h-full w-full object-cover object-center"
 											/>
--- src/pages/matches.tsx
+++ src/pages/matches.tsx
@@ -1,3 +1,4 @@
+import MatchInterface from "@/components/MatchInterface";
 import Matchbar from "@/components/Matchbar";
 
 import { useState } from "react";
@@ -6,7 +7,10 @@ export default function Matches() {
 	const [index, setIndex] = useState<number>(-1);
 	return (
 		<main className={"grow flex flex-col justify-center w-screen"}>
+			<div className="flex flex-col mx-14 lg:mx-20">
+				<MatchInterface />
 			<Matchbar index={index} setIndex={setIndex} items={[1,2,3,1,2,2,1,1,1,1,2,2,2,3]}/>
+			</div>
 		</main>
 	)
 }
