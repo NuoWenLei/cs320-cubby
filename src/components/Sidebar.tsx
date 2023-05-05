5714fec057d32ce007237779fd6ccc5c4ecdacd8
NuoWenLei <nuowen0612@gmail.com>
2023-05-05 19:23:40 -0400

    toggle group, make background color change for landing page

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,6 @@
 import { Group } from "@/utils/types";
 import GroupItem from "./GroupItem";
+import { useState } from "react";
 
 interface SidebarProps {
 	index: number | undefined;
@@ -10,14 +11,29 @@ interface SidebarProps {
 export default function Sidebar(
 	{ index, setIndex, items } : SidebarProps
 ) {
+
+	const [friendBar, setFriendBar] = useState<boolean>(true);
+
 	return (
 		<div className="grow flex flex-col py-5">
 			<div className="flex flex-row justify-center text-orange-900 text-3xl mb-4">
 				Groups
 			</div>
+			<div className="flex flex-row w-full text-white justify-around">
+				<button type="button" className={"p-2 m-2 rounded-full border border-2 border-orange-900 "
+				+ ((friendBar) ? "bg-orange-900" : "bg-transparent")}
+				onClick={() => setFriendBar(true)}>
+					Friend Groups
+				</button>
+				<button type="button" className={"p-2 m-2 rounded-full border border-2 border-orange-900 "
+				+ ((!friendBar) ? "bg-orange-900" : "bg-transparent")}
+				onClick={() => setFriendBar(false)}>
+					Communities
+				</button>
+			</div>
 			
 			{
-				items.map((g: Group, i: number) => {
+				items.filter((group: Group) => group.friend_group == friendBar).map((g: Group, i: number) => {
 					return (
 						<div
 						key={i}
