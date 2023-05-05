6ac1aa5c6ad3b845a6c70c41033f28023a11e144
Dv-song <seung_heon_song@brown.edu>
2023-05-05 01:36:48 -0400

    logo and ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,30 @@
-import { AuthState, useAuth } from '@/utils/firebaseFunctions';
-import Link from 'next/link';
+import { AuthState, useAuth } from "@/utils/firebaseFunctions";
+import Link from "next/link";
 
 export default function Home() {
   const auth: AuthState = useAuth();
   return (
     <main className={"grow flex flex-col justify-center "}>
-
-        <div className="text-orange-900 font-semibold text-4xl mx-auto mb-3">
-          Welcome to Cubby!
-        </div>
-        <div className="h-80 w-80 mx-auto mb-3">
-          <img
-            src="https://artprojectsforkids.org/wp-content/uploads/2021/06/Draw-a-Bear.jpg"
-            className="h-full w-full object-cover object-center"
-          />
-        </div>
-        <div className="mx-auto text-orange-900 text-lg">
-          {
-            auth.isAuthenticated ?
-            (
-              <Link href="/matches">
-              Start matching <span>&#8594;</span>
-            </Link>
-            ) : <Link href="/signup">
-                  Start matching <span>&#8594;</span>
-                </Link>
-          }
-        </div>
-      
+      <div className="text-orange-900 font-semibold text-4xl mx-auto mb-3">
+        Welcome to Cubby!
+      </div>
+      <div className="h-80 w-80 mx-auto mb-3">
+        <img
+          src="/imgs/CubbyBearWithLogo.png"
+          className="h-full w-full object-cover object-center"
+        />
+      </div>
+      <div className="mx-auto text-orange-900 text-lg">
+        {auth.isAuthenticated ? (
+          <Link href="/matches">
+            Start matching <span>&#8594;</span>
+          </Link>
+        ) : (
+          <Link href="/signup">
+            Start matching <span>&#8594;</span>
+          </Link>
+        )}
+      </div>
     </main>
   );
 }
